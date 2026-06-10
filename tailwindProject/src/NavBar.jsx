162751bf613623a8e7aea04cc648bf0575f30c9b
Shane <97196288+Lanternray@users.noreply.github.com>
2026-06-10 14:57:08 -0500

    Layout finish

---
+++ tailwindProject/src/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const listItems =[
+    'Work',
+    'Services',
+    'Pricing',
+    'Blog',
+    'Templates',
+    'About'
+]
+
+const NavBar = () => {
+    return (
+        <div className="fixed bg-black/60 flex justify-between items-center gap-16 py-3 px-10 left-1/2 translate-x-[-50%] top-[20px] rounded-full backdrop-blur-md text-white shadow-lg z-10">
+
+            <ul className="flex gap-8 text-xl">
+                {listItems.map((item) => (
+                    <li className="relative group cursor-pointer" key={item}>
+                        {item}
+                        <span className="absolute left-0 bottom-[-5px] w-0 h-1 rounded-xl bg-gradient-to-r from-blue-500 to-green-500 transition-all duration-300 group-hover:w-full"></span>
+                    </li>
+                ))}
+            </ul>
+
+            <button className="bg-gradient-to-r from-blue-500 to-green-400 py-1 px-6 rounded-3xl shadow-2xl text-white text-lg font-semibold hover:from-blue-600 hover:to-green-500 hover:shadow-green-500 shadow-blue-500">Contact</button>
+        </div>
+    )
+}
+
+export default NavBar
