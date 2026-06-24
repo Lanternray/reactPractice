3096f6c82e299eea61a6d597c10265570407bc06
Shane <97196288+Lanternray@users.noreply.github.com>
2026-06-24 10:09:20 -0500

    Project files update

--- tailwindProject/src/App.jsx
+++ tailwindProject/src/App.jsx
@@ -4,22 +4,7 @@ function App() {
 
   return (
     <>
-
-    <NavBar />
-
-    <div className="min-h-screen w-full relative bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400 flex items-center justify-center text-center flex-col gap-10 text-white p-52">
-      <h1 className="text-7xl">Section 1</h1>
-      <p className="text-3xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, cupiditate. Nostrum perferendis doloribus illum eos, consequatur mollitia, asperiores expedita odio, reiciendis amet iste eaque veritatis sunt fuga. Velit, inventore expedita!</p>
-    </div>
-    <div className="min-h-screen w-full relative bg-gradient-to-r bg-gradient-to-r from-orange-500 to-yellow-300 flex items-center justify-center text-center flex-col gap-10 text-white p-52">
-      <h1 className="text-7xl">Section 2</h1>
-      <p className="text-3xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, cupiditate. Nostrum perferendis doloribus illum eos, consequatur mollitia, asperiores expedita odio, reiciendis amet iste eaque veritatis sunt fuga. Velit, inventore expedita!</p>
-    </div>
-    <div className="min-h-screen w-full relative bg-gradient-to-r bg-gradient-to-r from-sky-500 to-blue-300 flex items-center justify-center text-center flex-col gap-10 text-white p-52">
-      <h1 className="text-7xl">Section 3</h1>
-      <p className="text-3xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, cupiditate. Nostrum perferendis doloribus illum eos, consequatur mollitia, asperiores expedita odio, reiciendis amet iste eaque veritatis sunt fuga. Velit, inventore expedita!</p>
-    </div>
-
+    
     </>
   )
 }
